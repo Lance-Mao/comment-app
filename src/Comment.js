765319e7f67dc2666ac5a3eb920983cb982bc518
Lance-Mao <977986310@qq.com>
2018-04-23 14:26:09 +0800

    添加代码块

--- src/Comment.js
+++ src/Comment.js
@@ -24,6 +24,10 @@ class Comment extends Component {
         )
     }
 
+    componentWillUnmount () {
+        clearInterval(this._timer)
+    }
+
     _updateTimeString() {
         const comment = this.props.comment;
         const duration = (+Date.now() - comment.createdTime) / 1000;
@@ -31,6 +35,16 @@ class Comment extends Component {
             timeString: duration > 60 ? `${Math.round(duration / 60)} 分钟前`
                 : `${Math.round(Math.max(duration, 1))} 秒前`
         })
+    }
+
+    _getProcessedContent(content){
+        return content
+            .replace(/&/g, "&amp;")
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;")
+            .replace(/"/g, "&quot;")
+            .replace(/'/g, "&#039;")
+            .replace(/`([\S\s]+?)`/g, '<code>$1</code>')
     }
 
     handleDeleteComment(){
@@ -45,7 +59,9 @@ class Comment extends Component {
                 <div className="comment-user">
                     <span>{this.props.comment.username}</span>
                 </div>
-                <p>{this.props.comment.content}</p>
+                <p dangerouslySetInnerHTML={{
+                    __html: this._getProcessedContent(this.props.comment.content)
+                }} />
                 <span className='comment-createdtime'>{this.state.timeString}</span>
                 <span className='comment-delete' onClick={this.handleDeleteComment.bind(this)}>删除</span>
             </div>
